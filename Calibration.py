463cd3bc9918f2fd19b4862edcfd9a297f0d3ecf
Callum Gilchrist <43007634+CallumGilly@users.noreply.github.com>
2023-06-01 15:40:04 +0100

    Move user interactions to Main

--- Calibration.py
+++ Calibration.py
@@ -12,12 +12,3 @@ def calibrate(alt, needed_altitudes, adjustments):
 
     return correction[0] + (alt - needed_altitudes[0]) * (correction[1] - correction[0]) / (
             needed_altitudes[1] - needed_altitudes[0])
-
-
-def calibrate():
-    altitude = int(input("Enter altitude: "))
-    needed_altitudes = get_alts_needed(altitude)
-    adj1 = int(input("Adj at " + str(needed_altitudes[0]) + "ft: "))
-    adj2 = int(input("Adj at " + str(needed_altitudes[1]) + "ft: "))
-
-    print(str(calibrate(altitude, needed_altitudes, [adj1, adj2])) + "ft")
--- Main.py
+++ Main.py
@@ -1,12 +1,25 @@
 import Calibration
 
+
+def calibrate():
+    altitude = int(input("Enter altitude: "))
+    needed_altitudes = Calibration.get_alts_needed(altitude)
+    adj1 = int(input("Adj at " + str(needed_altitudes[0]) + "ft: "))
+    adj2 = int(input("Adj at " + str(needed_altitudes[1]) + "ft: "))
+
+    print(str(Calibration.calibrate(altitude, needed_altitudes, [adj1, adj2])) + "ft")
+
+
 while True:
+    print("\n====================")
+    print("1) Calibration")
     try:
-        print("1) Calibration")
         option = int(input("Select prog: "))
         if option == 1:
-            Calibration.calibrate()
+            calibrate()
     except ValueError:
         print("Invalid input");
         pass
+
+
 
